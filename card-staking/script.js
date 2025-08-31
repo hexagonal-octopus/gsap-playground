c127f3dd494912b4f5e8c24819fee8cd3153cc9a
hexagonal.octopus <mickie.prasetya@gmail.com>
2025-08-31 14:56:58 +0700

    update staking box with gsap

--- card-staking/script.js
+++ card-staking/script.js
@@ -1,120 +1,70 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger, SplitText);
+  gsap.registerPlugin(ScrollTrigger);
 
-  document.fonts.ready.then(() => {
-    gsap.set("[text-split]", { opacity: 1 });
+  const scrollSection = document.querySelectorAll(".scroll-section");
 
-    document.querySelectorAll("[text-split]").forEach((el) => {
-      new SplitText(el, {
-        types: "words, chars, lines",
-        tag: "span",
-        wordsClass: "word",
-        charsClass: "char",
-        mask: "word",
-      });
-    });
+  scrollSection.forEach((section) => {
+    const wrapper = section.querySelector(".wrapper");
+    const items = wrapper.querySelectorAll(".list_item");
 
-    // slide words up animation
-    document.querySelectorAll("[words-slide-up]").forEach((el) => {
-      let tl = gsap.timeline({ paused: true });
-      tl.from(el.querySelectorAll(".word"), {
-        opacity: 0,
-        yPercent: 100,
-        duration: 0.5,
-        ease: "back.out(2)",
-        stagger: { amount: 0.6 },
-      });
-      createScrollTrigger(el, tl);
-    });
+    // initialize
+    let direction = null;
 
-    document.querySelectorAll("[words-rotate-in]").forEach((el) => {
-      let tl = gsap.timeline({ paused: true });
-      tl.set(el.querySelectorAll(".word"), { transformPerspective: 1000 });
-
-      tl.from(el.querySelectorAll(".word"), {
-        rotationX: -90,
-        duration: 0.6,
-        ease: "power2.out",
-        stagger: { amount: 0.6 },
-      });
-
-      createScrollTrigger(el, tl);
-    });
-
-    document.querySelectorAll("[words-slides-from-right]").forEach((el) => {
-      let tl = gsap.timeline({ paused: true });
-
-      tl.from(el.querySelectorAll(".word"), {
-        opacity: 0,
-        x: 50,
-        duration: 0.6,
-        ease: "power2.out",
-        stagger: { amount: 0.2 },
-      });
-
-      createScrollTrigger(el, tl);
-    });
-
-    document.querySelectorAll("[letters-fade-in]").forEach((el) => {
-      let tl = gsap.timeline({ paused: true });
-
-      tl.from(el.querySelectorAll(".char"), {
-        opacity: 0,
-        duration: 0.2,
-        ease: "power1.out",
-        stagger: { amount: 0.8 },
-      });
-
-      createScrollTrigger(el, tl);
-    });
-
-    document.querySelectorAll("[letters-fade-in-random]").forEach((el) => {
-      let tl = gsap.timeline({ paused: true });
-
-      tl.from(el.querySelectorAll(".char"), {
-        opacity: 0,
-        duration: 0.1,
-        ease: "power1.out",
-        stagger: { amount: 0.4, from: "random" },
-      });
-
-      createScrollTrigger(el, tl);
-    });
-
-    document.querySelectorAll("[scrub-each-word]").forEach((el) => {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: el,
-          start: "top center",
-          end: "center top",
-          scrub: true,
-        },
-      });
-
-      tl.from(el.querySelectorAll(".word"), {
-        opacity: 0.2,
-        duration: 0.2,
-        ease: "power1.out",
-        stagger: { each: 0.4 },
-      });
-    });
-
-    function createScrollTrigger(triggerElement, timeline) {
-      ScrollTrigger.create({
-        trigger: triggerElement,
-        start: "top bottom",
-        markers: true,
-        onLeaveBack: () => {
-          timeline.progress(0).pause();
-        },
-      });
-
-      ScrollTrigger.create({
-        trigger: triggerElement,
-        start: "top center",
-        markers: true,
-        onEnter: () => timeline.play(),
-      });
+    if (section.classList.contains("vertical-section")) {
+      direction = "vertical";
+    } else if (section.classList.contains("horizontal-section")) {
+      direction = "horizontal";
     }
+
+    initScroll(section, items, direction);
   });
+
+  function initScroll(section, items, direction) {
+    // initial state
+    items.forEach((item, index) => {
+      if (index !== 0) {
+        direction === "horizontal"
+          ? gsap.set(item, { xPercent: 100 })
+          : gsap.set(item, { yPercent: 100 });
+      }
+    });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        pin: true,
+        start: "top top",
+        end: () => `+=${items.length * 100}%`,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        // markers: true,
+      },
+      defaults: {
+        ease: "none",
+      },
+    });
+
+    items.forEach((item, index) => {
+      timeline.to(item, {
+        scale: 0.8,
+        borderRadius: "20px",
+      });
+
+      direction == "horizontal"
+        ? timeline.to(
+            items[index + 1],
+            {
+              xPercent: 0,
+            },
+            "<"
+          )
+        : timeline.to(
+            items[index + 1],
+            {
+              yPercent: 0,
+            },
+            "<"
+          );
+    });
+  }
 });
